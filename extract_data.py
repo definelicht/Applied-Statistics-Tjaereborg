2e730101f636bd763f42b947e448f395a7c544d3
Johannes de Fine Licht <johannes@musicmedia.dk>
2013-10-16 15:03:34 +0200

    Pretty plots!

--- extract_data.py
+++ extract_data.py
@@ -10,8 +10,12 @@ filename = folder + "all.data"
 
 data = read_csv.ReadCsv(filename)
 lines = len(data)
-outfile = open(folder + "windspeed.data","w")
-outfile.write("t,h=17m,h=28.5m,h=41m,h=57m,h=77m,h=90m")
+windspeed_file = open(folder + "windspeed.data","w")
+windspeed_file.write("t,h=17m,h=28.5m,h=41m,h=57m,h=77m,h=90m")
+sonic_file = open(folder + "sonic.data","w")
+sonic_file.write("t,h=17m,h=77m,h=90m")
 for i in range(lines):
-  outfile.write("\n%f,%f,%f,%f,%f,%f,%f" % (data[i,173],data[i,117],data[i,116],data[i,115],data[i,114],data[i,113],data[i,112]))
-outfile.close();
+  windspeed_file.write("\n%f,%f,%f,%f,%f,%f,%f" % (data[i,173],data[i,117],data[i,116],data[i,115],data[i,114],data[i,113],data[i,112]))
+  sonic_file.write("\n%f,%f,%f,%f" % (data[i,173],data[i,109],data[i,101],data[i,93]))
+windspeed_file.close()
+sonic_file.close()
